6e8102737e55a68a2bb78dc1356895f7d883b66f
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2024-03-14 14:26:06 -0300

    build: nextjs configuration
    logging and styled components options to true

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  logging: {
+    fetches: {
+      fullUrl: true
+    }
+  }
 }
 
 module.exports = nextConfig
