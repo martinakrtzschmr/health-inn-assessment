183260c7f0bfca4e09ecc363969f7d7fcbb5f0d7
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2024-03-16 22:02:58 -0300

    style: es-lint formatting

--- src/styles/pages/User.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import colors from '../colors';
-
-export const CardStyle = styled.div`
-  padding: 15px;
-  background-color: ${colors.white};
-  border-radius: 3px;
-  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
-`;
