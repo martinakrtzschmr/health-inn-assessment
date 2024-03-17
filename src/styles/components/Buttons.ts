02f305b752b98009918ef9e732875930ad2d333c
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2024-03-16 22:01:52 -0300

    feat: Add Button styles

---
+++ src/styles/components/Buttons.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import colors from '../colors';
+
+export const DeleteButton = styled.button`
+  border: 1px solid lightgray;
+  border-radius: 6px;
+  padding: 5px;
+  
+  background-color: ${colors.confortableRedRbg};
+  color: ${colors.white};
+  transition-property: color, background-color, border-color, text-decoration-color, fill, stroke;
+  transition-duration: 150ms;
+  &:hover {
+    background-color: ${colors.lightGray};
+  }
+`;
+
+export const EditButton = styled.button`
+  border: 1px solid lightgray;
+  border-radius: 6px;
+  padding: 5px;
+  
+  background-color: ${colors.solidYellow};
+  color: ${colors.white};
+  transition-property: color, background-color, border-color, text-decoration-color, fill, stroke;
+  transition-duration: 150ms;
+  &:hover {
+    background-color: ${colors.lightGray};
+  }
+`;
+
+export const ButtonOutline = styled.button`
+  border: 1px solid ${colors.solidYellow};
+  background-color: ${colors.white};
+  color: ${colors.solidYellow};
+
+  border-radius: 10px;
+  padding: 5px;
+
+  transition-property: color, background-color, border-color, text-decoration-color, fill, stroke;
+  transition-duration: 150ms;
+
+  &:hover {
+    background-color: ${colors.solidYellow};
+    color: ${colors.white}
+  }
+`;
