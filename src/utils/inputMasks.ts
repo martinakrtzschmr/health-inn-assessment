d4c847e3fb4e4cab5277f976948613e636b36aa2
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2024-03-16 22:03:22 -0300

    feat: Add CPF mask input

--- src/utils/inputMasks.ts
+++ src/utils/inputMasks.ts
@@ -15,3 +15,12 @@ export function cnpjMask(value) {
     .replace(/(\d{4})(\d{1,2})/, "$1-$2")
     .replace(/(-\d{2})\d+?$/, "$1");
 }
+
+export function cpfMask(value: string) {
+  return value
+    .replace(/\D/g, "")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d{1,2})/, "$1-$2")
+    .replace(/(-\d{2})\d+?$/, "$1");
+}
