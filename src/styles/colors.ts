caf63bb127c82d0e4e694e91bdca757b76a43ed1
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2024-03-16 22:01:29 -0300

    feat: Add new colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,4 +1,5 @@
 const colors = {
+  black: "#000000",
   white: "#ffffff",
   mostlyWhite: "#f5f5f5",
   lightGray: "#cccccc",
@@ -10,6 +11,12 @@ const colors = {
   strongCyan: "#27b5bf",
   vividBlue: "#17a9fb",
   darkBlue: "#023D68",
+  confortableRedRbg: "rgb(219, 36, 36)",
+  confortableRed: "#df2020",
+  solidYellow: "rgb(249, 112, 21)",
+
+  primary: "222.2 47.4% 11.2%",
+  primaryForeground: "210 40% 98%",
 };
 
 export default colors;
