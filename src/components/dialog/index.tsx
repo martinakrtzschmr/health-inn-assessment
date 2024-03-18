5c1e59c12e68a183c3777c98864bc5267b17cd77
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2024-03-18 20:34:22 -0300

    feat: create Dialog component

---
+++ src/components/dialog/index.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import { ButtonOutline } from '../../styles/components/Buttons';
+import {
+  DialogBackground,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogWrapper,
+} from '../../styles/components/Dialog';
+
+interface IDialog extends React.ComponentPropsWithoutRef<'div'> {
+  title: string;
+  openButtonLabel: string;
+  closeButtonLabel: string;
+}
+
+const Dialog: React.FC<IDialog> = ({
+  title,
+  openButtonLabel,
+  closeButtonLabel,
+  children,
+}) => {
+  const [open, setOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <>
+      <ButtonOutline onClick={toggleOpen}>{openButtonLabel}</ButtonOutline>
+
+      {open && (
+        <DialogBackground>
+          <DialogWrapper>
+            <DialogHeader>
+              <DialogTitle>{title}</DialogTitle>
+            </DialogHeader>
+            <DialogContent>{children}</DialogContent>
+            <DialogFooter>
+              <ButtonOutline onClick={toggleOpen}>
+                {closeButtonLabel}
+              </ButtonOutline>
+            </DialogFooter>
+          </DialogWrapper>
+        </DialogBackground>
+      )}
+    </>
+  );
+};
+
+export default Dialog;
