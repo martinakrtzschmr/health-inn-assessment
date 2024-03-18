5631df5edcdb64f16d031fc76c9a974f9ba22cfe
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2024-03-18 20:36:06 -0300

    feat: update services list
    API request must include filtering options and error treatment

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,35 +1,74 @@
-import { User } from '../types';
+import { User, UserPostData } from '../types';
 
 export const getUsers = async (): Promise<User[]> => {
-  const response = await fetch('http://localhost:3003/users?_embed=companie&_embed=department');
-  const data: User[] = await response.json();
-  return data;
+  try {
+    const response = await fetch(
+      'http://localhost:3003/users/?_embed=companie&_embed=department&active=true'
+    );
+
+    if (!response.ok) {
+      throw new Error('Could not fetch resource');
+    }
+
+    const data: User[] = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const getUser = async (id: string): Promise<User> => {
-  const response = await fetch(`http://localhost:3003/users/${id}`);
-  const data: User = await response.json();
-  return data;
+  try {
+    const response = await fetch(`http://localhost:3003/users/${id}/?_embed=companie&_embed=department&active=true`);
+
+    if (!response.ok) {
+      throw new Error('Could not fetch resource');
+    }
+
+    const data: User = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const addUser = async (user: User): Promise<User> => {
-  const response = await fetch('http://localhost:3003/users', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(user),
-  });
-  const data: User = await response.json();
-  return data;
+  try {
+    const response = await fetch('http://localhost:3003/users', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user),
+    });
+
+    if (!response.ok) {
+      throw new Error('Could not fetch resource');
+    }
+
+    const data: User = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-export const editUser = async (user: User): Promise<User> => {
-  const response = await fetch(`http://localhost:3003/users/${user.id}`, {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(user),
-  });
-  const data: User = await response.json();
-  return data;
+export const editUser = async (user: UserPostData): Promise<UserPostData> => {
+  console.log('user: ', user);
+  try {
+    const response = await fetch(`http://localhost:3003/users/${user.id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user),
+    });
+
+    if (!response.ok) {
+      throw new Error('Could not fetch resource');
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const deleteUser = async (id: string): Promise<User> => {
