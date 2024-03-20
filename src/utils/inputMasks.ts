e7b5774fe382be82b731ce71e5ebd287b5e8c4b8
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2024-03-20 14:59:14 -0300

    feat: create rgMask function

--- src/utils/inputMasks.ts
+++ src/utils/inputMasks.ts
@@ -24,3 +24,12 @@ export function cpfMask(value: string) {
     .replace(/(\d{3})(\d{1,2})/, "$1-$2")
     .replace(/(-\d{2})\d+?$/, "$1");
 }
+
+export function rgMask(value: string) {
+  return value
+    .replace(/\D/g, "")
+    .replace(/(\d{2})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d{1,2})/, "$1-$2")
+    .replace(/(-\d{2})\d+?$/, "$1");
+}
